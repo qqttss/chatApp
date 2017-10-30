d87be33444234d00ae107f3955e136dfff990223
Quantong Shen <quantong.shen@HCL.COM>
2017-10-30 15:33:41 -0400

    Chat app

---
+++ public/js/chat.js
@@ -0,0 +1,108 @@
+// initilize request
+var socket = io();
+
+function scrollToBottom() {
+  //  Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  //  Heights
+  var clientHeight = messages.prop('clientHeight');;
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
+socket.on('connect', function() {
+  //console.log('Connected to the server.');
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function(err) {
+    if (err) {
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('no errro');
+    }
+  });
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnected from server.');
+});
+
+socket.on('updateUserList', function(users) {
+  console.log('Users list: ', users);
+
+  var ol = jQuery('<ol></ol>');
+  users.forEach(function(user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
+});
+
+socket.on('newMessage', function(message) {
+  //console.log('New message: ', message);
+
+  var formattedTime = moment(message.createAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  scrollToBottom();
+});
+
+socket.on('newLocationMessage', function(message) {
+  var formattedTime = moment(message.createAt).format('h:mm a');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+
+  jQuery('#messages').append(html);
+  scrollToBottom();
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  var messageTextbox = jQuery('[name=message]');
+
+  socket.emit('createMessage', {
+    text: messageTextbox.val()
+  }, function() {
+    messageTextbox.val('');
+  });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+  if (!navigator.geolocation) {
+    return console.log('Geolocation not supported by yout browser.');
+  }
+
+  locationButton.attr('disabled', 'disabled').text('Send location ...');
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.removeAttr('disabled').text('Send Location');
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function() {
+    locationButton.removeAttr('disabled').text('Send Location');
+    console.log('Unable to fetch location.');
+  });
+});
---
+++ server/utils/message.js
@@ -0,0 +1,24 @@
+//     generate message
+
+var moment = require('moment');
+
+var generateMessage = (from, text) => {
+  return {
+    from: from,
+    text: text,
+    createAt: moment().valueOf()
+  };
+};
+
+var generateLocationMessage = (from, latitude, longitude) => {
+  return {
+    from: from,
+    url: `https://www.google.com/maps?q=${latitude},${longitude}`,
+    createAt: moment().valueOf()
+  };
+};
+
+module.exports  = {
+  generateMessage: generateMessage,
+  generateLocationMessage: generateLocationMessage
+};
